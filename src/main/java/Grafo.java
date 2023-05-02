7ffea83e5e54856c6217dfd5eee1833eb1ba264e
Diego <diegocarrasco1013@gmail.com>
2023-05-02 22:50:42 +0200

    Grafo

--- src/main/java/Grafo.java
+++ src/main/java/Grafo.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Grafo {
+public class Grafo {
+
+    private int[][] adjacencyMatrix;
+    private int numVertices;
+
+    public Grafo(int numVertices) {
+        this.numVertices = numVertices;
+        this.adjacencyMatrix = new int[numVertices][numVertices];
+    }
+
+    public void addEdge(int i, int j, int weight) {
+        if (i != j) {
+            adjacencyMatrix[i][j] = weight;
+            adjacencyMatrix[j][i] = weight;
+        } else System.out.println("No se puede hacer un camino al mismo sitio");
+    }
+
+    public void removeEdge(int i, int j) {
+        adjacencyMatrix[i][j] = 0;
+        adjacencyMatrix[j][i] = 0;
+    }
+
+    public boolean hasEdge(int i, int j) {
+        return adjacencyMatrix[i][j] != 0;
+    }
+
+    public int getWeight(int i, int j) {
+        return adjacencyMatrix[i][j];
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
